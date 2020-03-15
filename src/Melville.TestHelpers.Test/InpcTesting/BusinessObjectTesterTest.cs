916e1ac529e80479df7eca8748a7850b2406ec75
John Melville <JohnMelville@gmail.com>
2020-03-15 18:05:57 -0400

    Moved a Unit Test Project Over

---
+++ src/Melville.TestHelpers.Test/InpcTesting/BusinessObjectTesterTest.cs
@@ -0,0 +1,127 @@
+﻿#nullable disable warnings
+using  System;
+using System.Reflection;
+using Melville.MVVM.BusinessObjects;
+using Melville.TestHelpers.Assertions;
+using Melville.TestHelpers.InpcTesting;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Melville.TestGoodies.Test.InpcTesting
+{
+  public sealed partial class BusinessObjectTesterTest
+  {
+    private class HasDefaultConstructor { }
+    public enum TryAnEnum { Zero, One, Two, Three};
+    private class DoesNotNotify:NotifyBase
+    {
+      public int IntProperty { get; set; }
+    }
+    [Fact]
+    public void ImputeSimpleProperties()
+    {
+      BusinessObjectTester.Create<MondoBusinessObject>().DoTests();
+    }
+
+    [Fact]
+    public void SerialieSucceed()
+    {
+      BusinessObjectTester.Create(()=>new MondoBusinessObject(), i=>i).DoTests();
+    }
+    [Fact]
+    public void SerialieFail()
+    {
+      Assert.Throws<AssertActualExpectedException>(()=>
+      BusinessObjectTester.Create(()=>new MondoBusinessObject(), i=>new MondoBusinessObject()).DoTests());
+    }
+
+
+    [Fact]
+    public void ImputedPropertyFailsInpc()
+    {
+      Assert.Throws<EqualException>(()=>BusinessObjectTester.Create<DoesNotNotify>().DoTests());
+    }
+    [Fact]
+    public void SimpleObjectFail()
+    {
+      Assert.Throws<EqualException>(()=>BusinessObjectTester.Create<DoesNotNotify>()
+        .Property(i=>i.IntProperty, 2)
+        .DoTests());
+    }
+
+    [Fact]
+    public void CheckSimpleObject()
+    {
+      BusinessObjectTester.Create<MondoBusinessObject>()
+        .Property(i => i.Int32Property, 12)
+        .DoTests();
+    }
+
+    private class NonDefaultConstructor:NotifyBase
+    {
+      public NonDefaultConstructor Other { get; set; }
+      public NonDefaultConstructor(int x) { }
+    }
+
+    [Fact]
+    public void CannotConstructField()
+    {
+      AssertEx.Throws<TrueException>("Other is not tested in the property test.\r\nExpected: True\r\nActual:   False", () => 
+          BusinessObjectTester.Create(()=>new NonDefaultConstructor(1)).DoTests());
+    }
+
+    [Fact]
+    public void IgnoreField()
+    {
+      BusinessObjectTester.Create(()=>new NonDefaultConstructor(1))
+        .ExcludedProperty(i=>i.Other)
+        .DoTests();
+    }
+
+    [Fact]
+    public void Explicit()
+    {
+      Assert.Throws<EqualException>(()=>
+      BusinessObjectTester.Create(()=>new NonDefaultConstructor(1))
+        .Property(i=>i.Other, new NonDefaultConstructor(2))
+        .DoTests());
+    }
+
+
+    private class DependantProp : NotifyBase
+    {
+      private int prop1;
+      public int Prop1
+      {
+        get => prop1;
+        set => AssignAndNotify(ref prop1, value, nameof(Prop1), nameof(Prop2), nameof(Prop3));
+      }
+
+      public int Prop2 { get; }
+      public int Prop3 { get; }
+
+    }
+
+    [Fact]
+    public void MultiPropFail()
+    {
+      Assert.Throws<TargetInvocationException>(()=>BusinessObjectTester.Create(() => new DependantProp()).DoTests());
+    }
+    [Fact]
+    public void MultiPropFail2()
+    {
+      Assert.Throws<TargetInvocationException>(()=>BusinessObjectTester.Create(() => new DependantProp())
+        .Property(i=>i.Prop1, 1, i=>i.Prop2)
+        .DoTests());
+    }
+
+    [Fact]
+    public void MultiPropSucceed()
+    {
+      BusinessObjectTester.Create(() => new DependantProp())
+        .Property(i=>i.Prop1, 1, i=>i.Prop2, i=>i.Prop3)
+        .DoTests();
+    }
+
+  }
+}
---
+++ src/Melville.TestHelpers.Test/MockConstruction/ConstructWithMockTest.cs
@@ -0,0 +1,211 @@
+﻿#nullable disable warnings
+using  System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Melville.TestHelpers.Assertions;
+using Melville.TestHelpers.MockConstruction;
+using Moq;
+using Xunit;
+
+namespace Melville.TestGoodies.Test.MockConstruction
+{
+  public sealed class CreateWithMocksTest
+  {
+    private class SimpleObject
+    {
+      public int Value { get; set; }
+
+      public SimpleObject(int value)
+      {
+        Value = value;
+      }
+    }
+
+    [Fact]
+    public void CanAddValueParam()
+    {
+      CreateWithMocks.Construct<SimpleObject>(12).InputValue<int>(out var val);
+      Assert.Equal(12, val);
+    }
+
+    [Fact]
+    public void CanAddValueParam2()
+    {
+      var val = CreateWithMocks.Construct<SimpleObject>(12).InputValue<int>();
+      Assert.Equal(12, val);
+    }
+
+    [Fact]
+    public void NoValueThrows()
+    {
+      AssertEx.Throws<InvalidOperationException>("No value of type String found.",
+        () => CreateWithMocks.Construct<SimpleObject>().InputValue<string>());
+    }
+
+    [Fact]
+    public void CreateSimpleObject()
+    {
+      var val = CreateWithMocks.Construct<SimpleObject>(10).Result(out var val2).Result();
+      Assert.Equal(10, val.Value);
+      Assert.Equal(10, val2.Value);
+      Assert.NotEqual(val, val2);
+    }
+
+    [Fact]
+    public void GenerateDefaultValueType()
+    {
+      var val = CreateWithMocks.Construct<SimpleObject>().InputValue(out int i).Result();
+      Assert.Equal(0, val.Value);
+      Assert.Equal(0, i);
+    }
+
+    public class WithInterface
+    {
+      public WithInterface(IDisposable value)
+      {
+        Value = value;
+      }
+
+      public IDisposable Value { get; }
+    }
+
+    [Fact]
+    public void ConsumeMock()
+    {
+      var mockIn = new Mock<IDisposable>();
+      var val = CreateWithMocks.Construct<WithInterface>(mockIn).Mock<IDisposable>(out var dispMock).Result();
+      Assert.Equal(mockIn, dispMock);
+      val.Value.Dispose();
+      mockIn.Verify(i => i.Dispose(), Times.Once);
+    }
+
+    [Fact]
+    public void CreateMock()
+    {
+      var mo = new Mock<IComparable<int>>();
+      var val = CreateWithMocks.Construct<WithInterface>().Mock<IDisposable>(out var dispMock).Result();
+      val.Value.Dispose();
+      dispMock.Verify(i => i.Dispose(), Times.Once);
+    }
+
+    public interface IInnerProp
+    {
+      IDisposable InnerProp { get; }
+    }
+
+    private class InnerNotify
+    {
+      public InnerNotify(IInnerProp prop)
+      {
+        Prop = prop;
+      }
+
+      public IInnerProp Prop { get; set; }
+    }
+
+    [Fact]
+    public void PropertyFilled()
+    {
+      var obj = CreateWithMocks.Construct<InnerNotify>().Mock<IDisposable>(out var disp).Result();
+      obj.Prop.InnerProp.Dispose();
+      disp.Verify(i => i.Dispose(), Times.Once);
+    }
+
+
+    private class HasEquatable
+    {
+      public HasEquatable(IEquatable<int> ints)
+      {
+        Ints = ints;
+      }
+
+      public IEquatable<int> Ints { get; }
+    }
+
+    [Fact]
+    public void CreateCompositeMock()
+    {
+      var m = CreateWithMocks.Construct<HasEquatable>().Mock<IEquatable<int>>(out var enumerator).Result();
+      enumerator.Setup(i => i.Equals(1)).Returns(true);
+     
+      Assert.True(enumerator.Object.Equals(1));
+      Assert.False(enumerator.Object.Equals(2));
+    }
+
+    public interface IDependendInterface
+    {
+      bool IsConcrete { get; }
+    }
+
+    private class DependantClass : IDependendInterface
+    {
+      public bool IsConcrete => true;
+    }
+
+    public interface IOuterInt
+    {
+      IDependendInterface Value { get; }
+    }
+
+    [Fact]
+    public void ComposeInterfaces()
+    {
+      Assert.False(CreateWithMocks.Mock<IOuterInt>().Result().Object.Value.IsConcrete);
+    }
+    [Fact]
+    public void ComposeWithConcrete()
+    {
+      Assert.True(CreateWithMocks.Mock<IOuterInt>(CreateWithMocks.Concrete<DependantClass>()).Result().Object.Value.IsConcrete);
+    }
+
+    public interface IHolder<T>
+    {
+      T Value { get; }
+    }
+
+    public class ConcreteClass<T>
+    {
+      public ConcreteClass(IHolder<T> holder) { }
+    }
+
+    public T GenerateT<T>()
+
+    {
+      CreateWithMocks.Construct<ConcreteClass<T>>()
+        .Mock<IHolder<T>>(out var holder);
+      return holder.Object.Value;
+    }
+    [Fact]
+    public void MockIntHolder()
+    {
+      Assert.Equal(0, GenerateT<int>());
+      
+    }
+
+    [Theory]
+    [InlineData(typeof(ArrayList), typeof(ArrayList))]
+    [InlineData(typeof(List<int>), typeof(List<int>))]
+    [InlineData(typeof(IList<int>), typeof(List<int>))]
+    [InlineData(typeof(IEnumerable<int>), typeof(List<int>))]
+    [InlineData(typeof(Collection<int>), typeof(Collection<int>))]
+    [InlineData(typeof(Stack<int>), typeof(Stack<int>))]
+    [InlineData(typeof(HashSet<int>), typeof(HashSet<int>))]
+    [InlineData(typeof(SortedDictionary<int, string>), typeof(SortedDictionary<int, string>))]
+    [InlineData(typeof(Dictionary<int, string>), typeof(Dictionary<int, string>))]
+    [InlineData(typeof(ICollection<int>), typeof(Collection<int>))]
+    [InlineData(typeof(ISet<int>), typeof(HashSet<int>))]
+    [InlineData(typeof(IDictionary<int, string>), typeof(Dictionary<int, string>))]
+    [InlineData(typeof(int[]), typeof(int[]))]
+    public void ConstructIntArray(Type source, Type dest)
+    {
+      var ret = GetType().GetMethods().First(i => i.Name == "GenerateT")
+        .MakeGenericMethod(new[] {source}).Invoke(this, new object[0]);
+      Assert.Equal(dest, ret.GetType());
+      
+    }
+
+
+  }
+}
