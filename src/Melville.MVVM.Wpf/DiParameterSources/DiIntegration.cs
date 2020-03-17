19a04ba75913caa1b086fa7e6677823d3338adfa
John Melville <JohnMelville@gmail.com>
2020-03-17 09:35:52 -0400

    Add logging and fix unit tests surrounding app level DI binding.

--- src/Melville.MVVM.Wpf/DiParameterSources/DiIntegration.cs
+++ src/Melville.MVVM.Wpf/DiParameterSources/DiIntegration.cs
@@ -8,10 +8,23 @@ namespace Melville.MVVM.Wpf.DiParameterSources
             typeof(IDIIntegration), typeof(DiIntegration),
             new FrameworkPropertyMetadata(new EmptyScopeFactory(), FrameworkPropertyMetadataOptions.Inherits));
 
+        private const string rootDiKey = "Melville.MVVM.WPF.IDIIntegration";
         public static IDIIntegration GetContainer(DependencyObject obj) =>
             (IDIIntegration) obj.GetValue(CoontainerProperty);
 
         public static void SetContainer(DependencyObject obj, IDIIntegration value) =>
             obj.SetValue(CoontainerProperty, value);
+        
+        public static IDIIntegration SearchForContainer(DependencyObject obj)
+        {
+            var ret = GetContainer(obj);
+            return ret is EmptyScopeFactory ? SearchApplicationRoot(Application.Current) : ret;
+        }
+
+        public static void AttachDiRoot(this Application app, IDIIntegration value) => 
+            app.Resources.Add(rootDiKey, value);
+
+        public static IDIIntegration SearchApplicationRoot(Application app) => 
+            app?.Resources[rootDiKey] as IDIIntegration ?? new EmptyScopeFactory();
     }
 }
