2fc180c6fb8117e692248163d4a55c2e93e859ef
John Melville <jmelvill@hotmail.com>
2020-03-27 17:22:42 -0400

    Add a wpf sample for the graphing library

---
+++ src/Melville.Wpf.Samples/LinqPadGraph/LinqGraphView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Melville.Wpf.Samples.LinqPadGraph    
+{
+    public partial class LinqGraphView : UserControl
+    {
+        public LinqGraphView()
+        {
+            InitializeComponent();
+        }
+    }
+}
