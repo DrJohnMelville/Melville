4151477f39b652c8205c6ad4080993775f4eacce
John Melville <JohnMelville@gmail.com>
2020-03-29 01:45:34 -0400

    Basic Shell for Logging UI

---
+++ Melville.Log.Viewer/HomeScreens/HomeScreenView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Melville.Log.Viewer.HomeScreens
+{
+    public partial class HomeScreenView : UserControl
+    {
+        public HomeScreenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
