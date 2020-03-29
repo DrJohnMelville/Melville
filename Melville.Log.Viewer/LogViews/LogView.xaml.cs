f1f0e719bd5510121ab1c0791adfb22257d4c59b
John Melville <JohnMelville@gmail.com>
2020-03-29 16:15:09 -0400

    Simple log works

---
+++ Melville.Log.Viewer/LogViews/LogView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Melville.Log.Viewer.LogViews
+{
+    public partial class LogView : UserControl
+    {
+        public LogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Melville.Log.Viewer/LogViews/LogViewModel.cs
+++ Melville.Log.Viewer/LogViews/LogViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Melville.Log.Viewer.HomeScreens;
 using Melville.Log.Viewer.NamedPipeServers;
+using Melville.MVVM.AdvancedLists;
 using Serilog.Events;
 using Serilog.Formatting.Compact.Reader;
 
@@ -11,28 +15,38 @@ namespace Melville.Log.Viewer.LogViews
     {
         public string Title { get; set; } = "Log Window";
         private readonly Stream logConnection;
+        public ICollection<LogEntryViewModel> Events { get; } = new ThreadSafeBindableCollection<LogEntryViewModel>();
 
         public LogViewModel(Stream logConnection)
         {
             this.logConnection = logConnection;
-        }
-        
-        public void Emit(LogEvent logEvent)
-        {
-//            LastLogEntry = logEvent.MessageTemplate.Render(logEvent.Properties);
+            ClientConnected();
         }
 
-        private void ClientConnected(object? sender, NewPipeConnectionEventArgs e)
+        private void ClientConnected()
         {
             Task.Run(() =>
             {
-                var reader = new LogEventReader(new StreamReader(e.ClientConnection));
+                var reader = new LogEventReader(new StreamReader(logConnection));
                 while (reader.TryRead(out var logEvent))
                 {
-                    Emit(logEvent);
+                    Events.Add(new LogEntryViewModel(logEvent));
                 }
             });
         }
+    }
 
+    public class LogEntryViewModel
+    {
+        private readonly LogEvent logEvent;
+        public LogEntryViewModel(LogEvent logEvent)
+        {
+            this.logEvent = logEvent;
+        }
+
+        public DateTimeOffset TimeStamp => logEvent.Timestamp;
+        public string Message => logEvent.MessageTemplate.Render(logEvent.Properties);
+        public LogEventLevel Level => logEvent.Level;
+        public string Exception => logEvent.Exception?.ToString();
     }
 }
