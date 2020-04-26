82e326a8e92119a4c04338662f5fbdad467209c0
John Melville <JohnMelville@gmail.com>
2020-04-26 16:20:32 -0400

    Sample website with a hub

---
+++ src/LoggingTestWebsite/Controllers/LoggingHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Serilog.Events;
+
+namespace LoggingTestWebsite.Controllers
+{
+    public class LoggingHub: Hub
+    {
+        public Task SetMinimumLogLevel(LogEventLevel minimumLevel)
+        {
+            return Clients.All.SendAsync("SendLogEvent", "Log EventData");
+        }
+    }
+}
