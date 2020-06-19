7cd1e5ecfc622bf4e8028cfe679c1c7a523d3a6a
John Melville <JohnMelville@gmail.com>
2020-06-19 04:59:57 -0400

    remove some junk

--- src/Melville.Mvvm.Test/Https/HttpClientServiceTest.cs
+++ src/Melville.Mvvm.Test/Https/HttpClientServiceTest.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Melville.Mvvm.TestHelpers.HttpServiceMocks;
 using Melville.MVVM.Https;
 using Moq;
 using Xunit;
