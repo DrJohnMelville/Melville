ba3f93b4614512b697bf98fcb29049c69ada70cd
John Melville <JohnMelville@gmail.com>
2020-11-07 05:38:19 -0500

    Generics and nullables generate correctly.

--- src/Melville.Generators.INPC/Common/AstUtilities/TypeSymbolOperations.cs
+++ src/Melville.Generators.INPC/Common/AstUtilities/TypeSymbolOperations.cs
@@ -12,83 +12,12 @@ namespace Melville.Generators.INPC.Common.AstUtilities
     {
         public static void WriteTypeSymbolName(this CodeWriter writer, ITypeSymbol symbol)
         {
-            AppendSymbolName(symbol, writer.Append);
+            writer.Append(symbol.ToString());
         }
 
         public static string FullyQualifiedName(this ITypeSymbol symbol)
         {
-            var sb = new StringBuilder();
-            AppendSymbolName(symbol, s=>sb.Append(s));
-            return sb.ToString();
-        }
-
-        private static void AppendSymbolName(ITypeSymbol symbol,
-            Action<string> appender)
-        {
-            appender(symbol.ToString());
-            // WriteContainingSymbolNames(appender, symbol);
-            // appender(symbol.Name);
-            // AppendTypeSuffixes(symbol as INamedTypeSymbol, appender);
-        }
-
-        private static void AppendTypeSuffixes(INamedTypeSymbol? symbol, Action<string> appender)
-        {
-            if (symbol == null) return;
-            TryAppendTypeArgumentList(symbol.TypeArguments, appender);
-            TryAppendNullableMark(symbol, appender);
-        }
-
-        private static void TryAppendNullableMark(INamedTypeSymbol symbol, Action<string> appender)
-        {
-            if (symbol.NullableAnnotation == NullableAnnotation.Annotated)
-            {
-                appender("?");
-            }
-        }
-
-        private static void TryAppendTypeArgumentList(
-            ImmutableArray<ITypeSymbol> arguments, Action<string> appender)
-        {
-            if (arguments.Length == 0) return;
-            appender("<");
-            CommaSeparatedSymbolList(arguments, appender);
-            appender(">");
-        }
-
-        private static void CommaSeparatedSymbolList(ImmutableArray<ITypeSymbol> arguments, Action<string> appender)
-        {
-            AppendSymbolName(arguments[0], appender);
-            foreach (var argument in arguments.Skip(1))
-            {
-                appender(",");
-                AppendSymbolName(argument, appender);
-            }
-        }
-
-        private static void WriteContainingSymbolNames(Action<string> appender, ITypeSymbol symbol)
-        {
-            foreach (var parentSymbol in ContainersOf(symbol).Reverse())
-            {
-                appender(parentSymbol.Name);
-                appender(ParentSymbolTerminator(parentSymbol));
-            }
-        }
-
-        private static string ParentSymbolTerminator(ISymbol parentSymbol) =>
-            parentSymbol switch
-            {
-                ITypeSymbol type => "+",
-                _=> "."
-            };
-
-        private static IEnumerable<ISymbol> ContainersOf(ITypeSymbol symbol)
-        {
-            var ret = symbol.ContainingSymbol;
-            while (!(ret is INamespaceSymbol ns && ns.IsGlobalNamespace))
-            {
-                yield return ret;
-                ret = ret.ContainingSymbol;
-            }
+            return symbol.ToString();
         }
     }
 }
