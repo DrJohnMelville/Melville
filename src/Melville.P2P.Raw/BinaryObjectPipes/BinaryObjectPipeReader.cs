c9b71f22e29f676d1804553c3093bf3c878b160b
John Melville <JohnMelville@gmail.com>
2021-01-17 07:12:33 -0500

    BinaryObjectPipeReader does not allocate a list in its read routine

---
+++ src/Melville.P2P.Raw/BinaryObjectPipes/BinaryObjectPipeReader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipelines;
+
+namespace Melville.P2P.Raw.BinaryObjectPipes
+{
+    public class BinaryObjectPipeReader
+    {
+        private readonly PipeReader reader;
+        private readonly BinaryObjectDictionary typeDictionary;
+    
+        public BinaryObjectPipeReader(Stream stream, BinaryObjectDictionary typeDictionary) :
+            this(PipeReader.Create(stream), typeDictionary)
+        {
+                    
+        }
+        public BinaryObjectPipeReader(PipeReader reader, BinaryObjectDictionary typeDictionary)
+        {
+            this.reader = reader;
+            this.typeDictionary = typeDictionary;
+        }  
+    
+        public async IAsyncEnumerable<object> Read()
+        {
+            ReadResult result;
+            do
+            {
+                result = await reader.ReadAsync();
+                var buffer = result.Buffer;
+                object? item;
+                SequencePosition finalPosition = buffer.Start;
+                do
+                {
+                    
+                    (item, finalPosition) = FirstObjectFromBuffer(buffer.Slice(finalPosition));
+                    if (item != null)
+                    {
+                        yield return item;
+                    }
+                    reader.AdvanceTo(finalPosition, buffer.End);
+                } while (item != null && ! finalPosition.Equals(buffer.End));
+            } while (!(result.IsCompleted || result.IsCanceled));
+        }
+                
+        private (object?, SequencePosition) FirstObjectFromBuffer(ReadOnlySequence<byte> input)
+        {
+            var byteReader = new SequenceReader<byte>(input);
+            var ret = ReadObject(ref byteReader);
+            return (ret, ret == null?input.Start:byteReader.Position);
+        }
+
+        private object? ReadObject(ref SequenceReader<byte> byteReader) =>
+            byteReader.TryRead(out var typeKey) &&
+            ReadOnjectByKey(typeKey, ref byteReader) is { } item
+                ? item
+                : null;
+
+        private object? ReadOnjectByKey(byte typeKey, ref SequenceReader<byte> byteReader) =>
+            typeDictionary.GetCreator(typeKey)(ref byteReader);
+    }
+}
---
+++ src/Melville.P2P.Raw/BinaryObjectPipes/BinaryObjectPipeWriter.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.IO.Pipelines;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Melville.P2P.Raw.BinaryObjectPipes
+{
+    
+    public class BinaryObjectPipeWriter
+    {
+        private readonly PipeWriter writer;
+        private readonly BinaryObjectDictionary typeDictionary;
+        
+        public BinaryObjectPipeWriter(PipeWriter writer, BinaryObjectDictionary typeDictionary)
+        {
+            this.writer = writer;
+            this.typeDictionary = typeDictionary;
+        }
+        
+        public BinaryObjectPipeWriter(Stream stream, BinaryObjectDictionary typeDictionary): 
+            this(PipeWriter.Create(stream), typeDictionary){}
+
+        public ValueTask<FlushResult> Write(ICanWriteToPipe value)
+        {
+            var mem = writer.GetMemory(1);
+            mem.Span[0] = typeDictionary.IndexOf(value.GetType());
+            writer.Advance(1);
+            value.WriteToPipe(writer);
+            return writer.FlushAsync();
+        }
+
+    }
+}
