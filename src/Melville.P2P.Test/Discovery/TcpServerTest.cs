6972a54308d7be6522c12f8aea53f91045bfee94
John Melville <jmelvill@hotmail.com>
2021-02-04 11:38:35 -0500

    remove a nextwork specific test

--- src/Melville.P2P.Test/Discovery/TcpServerTest.cs
+++ src/Melville.P2P.Test/Discovery/TcpServerTest.cs
@@ -11,17 +11,6 @@ namespace Melville.P2P.Test.Discovery
         private TcpServer server = new TcpServer();
 
         public void Dispose() => server.Dispose();
-
-        [Fact]
-        public void Address()
-        {
-            var addr = server.AddressArray();
-            Assert.Equal(6, addr.Length);
-            Assert.Equal(192, addr[0]);
-            Assert.Equal(168, addr[1]);
-            Assert.Equal(0, addr[2]);
-        }
-
         [Fact]
         public async Task ConnectTcp()
         {
