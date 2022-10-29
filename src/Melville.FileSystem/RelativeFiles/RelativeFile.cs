c29c278da02fc15f0a400b913066e645d90e2374
John Melville <JohnMelville@gmail.com>
2022-10-29 06:24:08 -0400

    Refactor RelativeDirectory

--- src/Melville.FileSystem/RelativeFiles/RelativeFile.cs
+++ src/Melville.FileSystem/RelativeFiles/RelativeFile.cs
@@ -1,10 +1,9 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
+using Melville.INPC;
 
 namespace Melville.FileSystem.RelativeFiles;
 
-public sealed class RelativeFile: FileAdapeterBase
+public sealed partial class RelativeFile: IFile
 {
   private readonly Func<IDirectory> concreteParent;
   private readonly string fileName;
@@ -14,33 +13,5 @@ public sealed class RelativeFile: FileAdapeterBase
     this.concreteParent = concreteParent;
     this.fileName = fileName;
   }
-  protected override IFile InnerFile() => concreteParent().File(fileName);
-}
-public abstract class FileAdapeterBase:IFile
-{
-  protected abstract IFile InnerFile();
-
-  #region MyRegion
-  public string Path => InnerFile().Path;
-  public string Name => InnerFile().Name;
-  public bool Exists() => InnerFile().Exists();
-  public Task<Stream> OpenRead() => InnerFile().OpenRead();
-  public Task<Stream> CreateWrite(FileAttributes attributes = FileAttributes.Normal) => 
-    InnerFile().CreateWrite(attributes);
-  public void Delete() => InnerFile().Delete();
-  public byte FinalProgress => InnerFile().FinalProgress;
-  public Task WaitForFinal => InnerFile().WaitForFinal;
-  public bool ValidFileSystemPath() => InnerFile().ValidFileSystemPath();
-  public IDirectory? Directory => InnerFile().Directory;
-  public long Size => InnerFile().Size;
-  public DateTime LastAccess
-  {
-    get => InnerFile().LastAccess;
-    set => InnerFile().LastAccess = value;
-  }
-  public DateTime LastWrite => InnerFile().LastWrite;
-  public DateTime Created => InnerFile().Created;
-  public FileAttributes Attributes => InnerFile().Attributes;
-
-  #endregion
+  [DelegateTo] private IFile InnerFile() => concreteParent().File(fileName);
 }
