c74a443c750021693c62feb783695b468b0c4d4a
John Melville <JohnMelville@gmail.com>
2023-09-12 14:01:04 -0400

    Fix some warnings.

---
+++ src/WebDashboard/NumberExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Melville.Hacks;
+
+public static class NumberExtensions
+{
+    /// <summary>
+    /// "Clamps" a value to be within the range of min...max inclusive
+    /// </summary>
+    /// <param name="value">the value to be clamped</param>
+    /// <param name="min">The minimum value</param>
+    /// <param name="max">the maximum value</param>
+    /// <returns>The initial value, clamped to the range of min...max</returns>
+    public static double Clamp(this double value, double min, double max) => Math.Max(min, Math.Min(max, value));
+
+    /// <summary>
+    /// "Clamps" a value to be within the range of min...max inclusive
+    /// </summary>
+    /// <param name="value">the value to be clamped</param>
+    /// <param name="min">The minimum value</param>
+    /// <param name="max">the maximum value</param>
+    /// <returns>The initial value, clamped to the range of min...max</returns>
+    public static int Clamp(this int value, int min, int max) => Math.Max(min, Math.Min(max, value));
+
+    /// <summary>
+    /// Returns true if Min &lt;= value &lt;= max
+    /// </summary>
+    /// <param name="value">the value to be clamped</param>
+    /// <param name="min">The minimum value</param>
+    /// <param name="max">the maximum value</param>
+    /// <returns>The initial value, clamped to the range of min...max</returns>
+    public static bool IsInRange(this double value, double min, double max) => (min <= value) && (value <= max);
+
+    /// <summary>
+    /// Returns true if Min &lt;= value &lt;= max
+    /// </summary>
+    /// <param name="value">the value to be clamped</param>
+    /// <param name="min">The minimum value</param>
+    /// <param name="max">the maximum value</param>
+    /// <returns>The initial value, clamped to the range of min...max</returns>
+    public static bool IsInRange(this int value, int min, int max) => (min <= value) && (value <= max);
+
+
+}
